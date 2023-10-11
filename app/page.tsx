2bedd6e32c5594dba822e212d4fed9b3623b698f
aidoskanapyanov <aidos.kanapyanov@alumni.nu.edu.kz>
2023-10-11 15:31:09 +0600

    Add dark mode toggle

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+import { ModeToggle } from "@/components/mode-toggle";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <ModeToggle />
+      <br />
       <Button>Click me</Button>
     </>
   );
