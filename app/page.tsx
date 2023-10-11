7a7a6287cbda9cbbd2c0fae0f6c5fd8f74693004
aidoskanapyanov <aidos.kanapyanov@alumni.nu.edu.kz>
2023-10-11 15:27:50 +0600

    Install shadcn/ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Button>Click me</Button>
+    </>
+  );
 }
