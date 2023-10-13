7f122a1b26ab42f8690d476ab8711021017a199e
aidoskanapyanov <aidos.kanapyanov@alumni.nu.edu.kz>
2023-10-13 16:35:57 +0600

    Style sign in modal

--- components/shared/sign-in-btn.tsx
+++ components/shared/sign-in-btn.tsx
@@ -1,3 +1,4 @@
+import { ChatGPT, Linkedin } from "@/components/shared/icons";
 import { Button } from "@/components/ui/button";
 import {
   Dialog,
@@ -7,6 +8,7 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
+import { Plus } from "lucide-react";
 
 const SignInBtn = () => {
   return (
@@ -16,14 +18,25 @@ const SignInBtn = () => {
           Sign in
         </Button>
       </DialogTrigger>
-      <DialogContent>
+      <DialogContent className="overflow-x-hidden p-10">
         <DialogHeader>
-          <DialogTitle>Are you sure absolutely sure?</DialogTitle>
-          <DialogDescription>
-            This action cannot be undone. This will permanently delete your
-            account and remove your data from our servers.
+          <DialogTitle className="flex flex-col items-center justify-center pb-4 text-center text-2xl font-bold">
+            <div className="flex items-center justify-center gap-1 pb-4">
+              <Linkedin className="h-8 w-8" />
+              <Plus />
+              <ChatGPT className="-m-2 h-12 w-12 fill-[#518275]" />
+            </div>
+            Sign in
+          </DialogTitle>
+          <DialogDescription className="text-center">
+            Sign in via your LinkedIn account to get started.
           </DialogDescription>
         </DialogHeader>
+        <div className="-mx-10 -mb-10 flex h-24 flex-col items-center justify-center border-t bg-gray-50">
+          <Button variant={"outline"} className="font-normal">
+            <Linkedin className="mr-2 h-4 w-4" /> Sign in with LinkedIn
+          </Button>
+        </div>
       </DialogContent>
     </Dialog>
   );
