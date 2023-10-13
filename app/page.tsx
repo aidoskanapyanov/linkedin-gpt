781696a9149f80f0e48d23c3007c30fb2331a53d
aidoskanapyanov <aidos.kanapyanov@alumni.nu.edu.kz>
2023-10-13 16:47:38 +0600

    Add navbar transition

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Title from "@/components/hero/title";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-start py-16">
+    <div className="flex min-h-screen flex-col items-center justify-start py-32">
       <div className="pb-8">
         <IntroducingTag />
       </div>
--- components/shared/nav.tsx
+++ components/shared/nav.tsx
@@ -1,12 +1,23 @@
-import { ChatGPT, Github, Linkedin } from "@/components/shared/icons";
+"use client";
+
+import { ChatGPT, Linkedin } from "@/components/shared/icons";
 import SignInBtn from "@/components/shared/sign-in-btn";
-import { Button } from "@/components/ui/button";
+import useScroll from "@/lib/hooks/use-scroll";
+import { cn } from "@/lib/utils";
 import { Plus } from "lucide-react";
 
 const Nav = () => {
+  const scrolled = useScroll(50);
   return (
-    <div className="mx-auto max-w-screen-xl">
-      <div className="flex items-center justify-between p-4">
+    <div
+      className={cn(
+        "fixed top-0 z-10 w-full transition-colors duration-300",
+        scrolled
+          ? "border-b border-gray-200 bg-white/50 backdrop-blur-xl"
+          : "bg-white/0",
+      )}
+    >
+      <div className="mx-auto flex max-w-screen-xl items-center justify-between p-4">
         <div className="flex items-center justify-center gap-1">
           <Linkedin className="h-8 w-8" />
           <Plus />
