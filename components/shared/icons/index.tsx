584de7472105a61ce9c7a19fbc90bf48f56f5768
aidoskanapyanov <aidos.kanapyanov@alumni.nu.edu.kz>
2023-10-13 11:46:39 +0600

    Add linkedin and ChatGPT icons

---
+++ components/shared/icons/index.tsx
@@ -0,0 +1,2 @@
+export { default as ChatGPT } from "./ChatGPT";
+export { default as Linkedin } from "./Linkedin";
--- components/shared/nav.tsx
+++ components/shared/nav.tsx
@@ -1,7 +1,16 @@
-import React from "react";
+import { ChatGPT, Linkedin } from "@/components/shared/icons";
+import { Plus } from "lucide-react";
 
 const Nav = () => {
-  return <div>nav</div>;
+  return (
+    <div>
+      <div className="flex items-center justify-center gap-1">
+        <ChatGPT className="-m-2 h-12 w-12" />
+        <Plus />
+        <Linkedin className="h-8 w-8" />
+      </div>
+    </div>
+  );
 };
 
 export default Nav;
