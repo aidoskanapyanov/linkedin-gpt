97c32568809e92b625239c19072a9ec8b1dead00
aidoskanapyanov <aidos.kanapyanov@alumni.nu.edu.kz>
2023-10-13 11:53:36 +0600

    Add github icon

--- components/shared/icons/index.tsx
+++ components/shared/icons/index.tsx
@@ -1,2 +1,3 @@
 export { default as ChatGPT } from "./ChatGPT";
 export { default as Linkedin } from "./Linkedin";
+export { default as Github } from "./Github";
--- components/shared/nav.tsx
+++ components/shared/nav.tsx
@@ -1,13 +1,17 @@
-import { ChatGPT, Linkedin } from "@/components/shared/icons";
+import { ChatGPT, Github, Linkedin } from "@/components/shared/icons";
 import { Plus } from "lucide-react";
 
 const Nav = () => {
   return (
-    <div>
+    <div className="flex items-center justify-between p-4">
       <div className="flex items-center justify-center gap-1">
         <ChatGPT className="-m-2 h-12 w-12" />
         <Plus />
         <Linkedin className="h-8 w-8" />
+        <span className="font-display text-2xl">LinkedinGPT</span>
+      </div>
+      <div>
+        <Github className="h-8 w-8" />
       </div>
     </div>
   );
