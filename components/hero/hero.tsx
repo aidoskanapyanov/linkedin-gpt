7400bf7540c7dcf1f55e451023a1cfdaec3423c5
aidoskanapyanov <aidos.kanapyanov@alumni.nu.edu.kz>
2023-10-16 09:24:18 +0600

    Add star on gh

--- components/hero/hero.tsx
+++ components/hero/hero.tsx
@@ -3,8 +3,11 @@
 import IntroducingTag from "@/components/hero/introducing-tag";
 import Subtitle from "@/components/hero/subtitle";
 import Title from "@/components/hero/title";
+import { Github } from "@/components/shared/icons";
+import { Button } from "@/components/ui/button";
 import { FADE_DOWN_ANIMATION_VARIANTS } from "@/lib/constants";
 import { motion } from "framer-motion";
+import Link from "next/link";
 
 const Hero = () => {
   return (
@@ -31,6 +34,19 @@ const Hero = () => {
       </motion.div>
       <motion.div variants={FADE_DOWN_ANIMATION_VARIANTS}>
         <Subtitle />
+      </motion.div>
+      <motion.div variants={FADE_DOWN_ANIMATION_VARIANTS}>
+        <Link
+          href={"https://github.com/aidoskanapyanov/linkedin-gpt"}
+          target="_blank"
+        >
+          <Button
+            variant={"outline"}
+            className="mt-6 rounded-full font-normal shadow-md"
+          >
+            <Github className="mr-2 h-6 w-6 fill-gray-600" /> Star on GitHub
+          </Button>
+        </Link>
       </motion.div>
     </motion.div>
   );
