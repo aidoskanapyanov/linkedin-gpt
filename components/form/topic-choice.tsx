71244f8ce0848a1eb01aa210de63f2cf82c26ef7
aidoskanapyanov <aidos.kanapyanov@alumni.nu.edu.kz>
2023-10-16 17:01:04 +0600

    Replace react-typed w/ typed.js

--- components/form/topic-choice.tsx
+++ components/form/topic-choice.tsx
@@ -1,32 +1,42 @@
 import { Textarea } from "@/components/ui/textarea";
-import Typed from "react-typed";
+import { useEffect, useRef } from "react";
+import Typed from "typed.js";
 
 const TopicChoice = () => {
+  const el = useRef(null);
+
+  useEffect(() => {
+    const typed = new Typed(el.current, {
+      strings: [
+        "What lessons can today's leaders learn from rise and fall of the Roman Empire?",
+        "The key factors that led to the rise of the Roman Empire.",
+        "The prosperity of ancient Rome and how they can be applied to modern businesses.",
+        "The Roman Empire's fall: a cautionary tale of overextension and internal strife.",
+        "From Julius Caesar to Augustus, we're exploring the iconic figures who shaped the Roman Empire's ascent.",
+        "Rome and the enduring legacy they leave on modern urban design.",
+        "The Roman Empire's military might.",
+      ],
+      typeSpeed: 10,
+      backSpeed: 10,
+      backDelay: 2500,
+      showCursor: false,
+      attr: "placeholder",
+      loop: true,
+    });
+
+    return () => {
+      // Destroy Typed instance during cleanup to stop animation
+      typed.destroy();
+    };
+  }, []);
+
   return (
     <div>
       <div className="pb-4 text-center text-lg font-semibold sm:text-2xl">
         What are you writing about?
       </div>
       <div className="mx-auto h-48 max-w-screen-md p-4">
-        <Typed
-          strings={[
-            "What lessons can today's leaders learn from rise and fall of the Roman Empire?",
-            "The key factors that led to the rise of the Roman Empire.",
-            "The prosperity of ancient Rome and how they can be applied to modern businesses.",
-            "The Roman Empire's fall: a cautionary tale of overextension and internal strife.",
-            "From Julius Caesar to Augustus, we're exploring the iconic figures who shaped the Roman Empire's ascent.",
-            "Rome and the enduring legacy they leave on modern urban design.",
-            "The Roman Empire's military might.",
-          ]}
-          typeSpeed={10}
-          backSpeed={10}
-          backDelay={2500}
-          showCursor={false}
-          attr="placeholder"
-          loop
-        >
-          <Textarea className="h-full p-8 text-lg" />
-        </Typed>
+        <Textarea className="h-full p-8 text-lg" ref={el} />
       </div>
     </div>
   );
