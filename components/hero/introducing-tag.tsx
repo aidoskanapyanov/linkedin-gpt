6529c509dcca99f48adb39f385ee0040ac2608d9
aidoskanapyanov <aidos.kanapyanov@alumni.nu.edu.kz>
2023-10-18 16:59:35 +0600

    Link to lin post 🔗

--- components/hero/introducing-tag.tsx
+++ components/hero/introducing-tag.tsx
@@ -1,13 +1,21 @@
 import { Linkedin } from "@/components/shared/icons";
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 import React from "react";
 
 const IntroducingTag = () => {
   return (
-    <Button className="rounded-full bg-blue-100 px-6 text-blue-700 hover:bg-blue-200">
-      <Linkedin className="mr-2 h-4 w-4" />
-      Introducing LinkedInGPT
-    </Button>
+    <Link
+      href={
+        "https://www.linkedin.com/posts/aidos-kanapyanov_linkedingpt-activity-7120362234316279808-Gn-t"
+      }
+      target="_blank"
+    >
+      <Button className="rounded-full bg-blue-100 px-6 text-blue-700 hover:bg-blue-200">
+        <Linkedin className="mr-2 h-4 w-4" />
+        Introducing LinkedInGPT
+      </Button>
+    </Link>
   );
 };
 
